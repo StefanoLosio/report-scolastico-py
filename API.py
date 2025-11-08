62da61b2da9f0974f971ccb002a384dfbf0b2663
Stefano <stefanolosio2008@gmail.com>
2025-11-08 16:00:29 +0100

    creata la sottoAPI e implementata la gestione del csv

--- API.py
+++ API.py
@@ -1,3 +1,5 @@
+import webview
+
 class api_primaria:
     def __init__(self, registro:list):
         self.registro=registro
@@ -7,20 +9,40 @@ class api_primaria:
     
     def aggiungiVoto(self, voto:dict):
         self.registro.append(voto)
+        db_voti = open("voti.csv", "a")
+        db_voti.write(f'\n{voto["id"]};{voto["voto"]};{voto["descrizione"]};{voto["data"]};{voto["materia"]}')
+        db_voti.close()
 
     def eliminaVoto (self, id:int):
+        db_voti = open("voti.csv", "w")
         i=0
         for voto in self.registro:
             if voto["id"]==id:
                 self.registro.pop(i)
+            else:
+                db_voti.write(f'{voto["id"]};{voto["voto"]};{voto["descrizione"]};{voto["data"]};{voto["materia"]}\n')
             i=i+1
+        db_voti.close()
     
     def modificaVoto (self, id:int, nuovo_voto:dict):
+        db_voti = open("voti.csv", "w")
         i=0
         for voto in self.registro:
             if voto["id"]==id:
                 self.registro[i]=nuovo_voto
             i=i+1
+            db_voti.write(f'{voto["id"]};{voto["voto"]};{voto["descrizione"]};{voto["data"]};{voto["materia"]}\n')
+        db_voti.close()
                 
 
-#class api_secondaria:
+class api_secondaria:
+    def __init__(self, registro:list):
+        self.registro=registro
+
+    def apriSubWindow(self, type:str):
+        if type=="inserimento":
+            window = webview.create_window("Inserisci Voto", "inserimento.html", width=400, height=600, js_api=self, resizable=False)
+            self.finestra=window
+        elif type=="modifica":
+            window = webview.create_window("Modifica Voto", "modifica.html", width=400, height=600, js_api=self, resizable=False)
+            self.finestra=window
